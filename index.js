7c7780f20044c8875f7240224f9030f75ce84cfa
Marc Newport <marcnewport@gmail.com>
2018-04-22 23:14:22 +1000

    added comments

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ require('dotenv').config();
 // Set constants
 const PORT = process.env.PORT || 3000;
 
+// Require packages
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+// Index route
 app.get('/', function(req, res) {
   res.sendFile(__dirname +'/views/index.html');
 });
 
+// Listen to socket connection
 io.on('connection', function(socket) {
   console.log('A user connected');
 
+  // Listen to socket message and fire event to browser
   socket.on('message', function(message){
     io.emit('message', message);
   });
